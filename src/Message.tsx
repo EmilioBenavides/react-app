b0e430efb664ddf3c8c4fe7ae320c1bfd347424d
Emilio <emilio.benavides68@gmail.com>
2025-03-03 23:03:30 -0600

    first commit

---
+++ src/Message.tsx
@@ -0,0 +1,12 @@
+
+//PascalCasing
+
+function Message() {
+    //JSX: JavaScript XML
+    const name = 'Emilio';
+    if (name) 
+        return <h1>Hello {name}</h1>;
+        return <h1>Hello World!</h1>;
+}
+
+export default Message;
