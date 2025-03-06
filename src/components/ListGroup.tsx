7227359f1f04059ab8061d92cd3eb59196815a86
Emilio <emilio.benavides68@gmail.com>
2025-03-05 23:22:14 -0600

    Added Click events in a component so you can see in the dev tools which item the user clicked on.

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -6,7 +6,6 @@ function ListGroup() {
     "Chicago",
     "Houston",
   ];
-  items = [];
 
   return (
     <>
@@ -14,7 +13,13 @@ function ListGroup() {
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item) => (
-          <li key={item}>{item}</li>
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={() => console.log(item)}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
