8765f2106b215a748a13b4b421811f3c39e57f28
Emilio <emilio.benavides68@gmail.com>
2025-03-13 23:38:20 -0500

    We add passing functions via props in the files.

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,17 @@ function App() {
     "Houston",
   ];
 
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
   return (
     <div>
-      <ListGroup items={items} heading="Cities" />
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
     </div>
   );
 }
